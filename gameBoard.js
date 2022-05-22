c863b4a864ecf02d132df7ffb1182f79a5c14475
Matthew Shane Nolan-Cintron <matthewnolancintron@gmai.com>
2022-05-22 14:59:48 -0400

    commented the requirements of the gameBoard function

---
+++ gameBoard.js
@@ -0,0 +1,25 @@
+function gameBoard(){
+    /**
+     * ability to place ships
+     * at specific coordinates 
+     * by calling the ship factory function
+     * 
+     * 
+     */
+
+    /**
+     * receiveAttack function:
+     * takes a pair of coordinates
+     * determines if the attack hit a ship
+     * sends hit function to the correct ship
+     * or records the coordinates of the missed shot
+     */
+
+    /**
+     * keep track of missed attacks and display them
+     */
+
+    /**
+     * report if all ships have been sunk
+     */
+}
