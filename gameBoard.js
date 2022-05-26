38555f0746bc4ef4ce1683fde522cc5faa224b0d
Matthew Shane Nolan-Cintron <matthewnolancintron@gmai.com>
2022-05-26 10:16:54 -0400

    implemented placeShip Method

--- gameBoard.js
+++ gameBoard.js
@@ -1,6 +1,9 @@
 const ship = require("./ship");
 function gameBoard() {
   return {
+    //an array of ships placed on the board
+    armada: [],
+
     /**
      * coordiantes:
      * an object with key value pairs
@@ -22,119 +25,141 @@ function gameBoard() {
         .flat(1)
     ),
 
-    //an array of ships placed on the board
-    yourShips:[],
-
     coordinatesOfEnemyShips: Object.fromEntries(
-        ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
-          .map((letter) =>
-            Array.from(Array(10).keys(), (x) => [
-              `${letter}${x + 1}`,
-              "unoccupied",
-            ])
-          )
-          .flat(1)
-      ),
-
-
+      ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+        .map((letter) =>
+          Array.from(Array(10).keys(), (x) => [
+            `${letter}${x + 1}`,
+            "unoccupied",
+          ])
+        )
+        .flat(1)
+    ),
 
     /**
      * ability to place ships
      * at specific coordinates
      * by calling the ship factory function
-     * 
-     * @
-     * TODO:implement this function check comments
-     * for building
      */
     placeShip(lengthOfShip, startingCoordinate, directionOfPlacement) {
-      //create a ship with length = lengthOfShip
-      let ship = ship(lengthOfShip);
+      let calculatedEndCoordinate;
+      let calculatedCoordinateRange;
+      let isOutOfBounds;
 
-      //need to update the coordiantes
-      //find coords
+      //calcualte range of cooridante ship will be 'placed' at
       if (directionOfPlacement == "horizontal") {
         /**
          * if placeing the ship horizontally
          * then the letter will stay the same
          * but the number will update
-         * if placing ship of length 2
-         * at a(1) then end location will be
-         * a(3) and the coordiantes of placment
-         * will be [a(1),a(2),a(3)]
-         * coords = from letter(x) to letter(x+ship.length)
-         * use this data structure to keep in the bound of
-         * the gameBoard [1,2,3,4,5,6,7,8,9,10]
-         * or just use a condtional to keep less than or
-         * equal to 10.
-         * also check if there is already a ship
-         * on any of the coords found
-         * if not then update those coords
-         * with ... not sure yet.
-         * also going to assume that the starting
+         *
+         * I'm going to assume that the starting
          * location when placing ship horizontally
          * is the ship's left most side such that
          * the rest of the ship is being place to the
          * right of that coordinate.
+         *
          */
+        let startingCoordianteNumber = parseInt(
+          startingCoordinate.split("")[1]
+        );
+
+        //find end point
+        calculatedEndCoordinate = startingCoordianteNumber + lengthOfShip;
+
+        /**
+         * check for out of bounds:
+         * due to position and length of ship
+         */
+        if (calculatedEndCoordinate > 10) {
+          isOutOfBounds = true;
+        }
+
+        if (isOutOfBounds) {
+          //return error or throw error
+          /**do error handling (need to learn how.) */
+          throw "out of bounds ship can't be placed there";
+        } else {
+          //create coordiante range
+          calculatedCoordinateRange = Array.from(Array(10).keys(), (x) => x + 1)
+            .slice(startingCoordianteNumber - 1, calculatedEndCoordinate)
+            .map((x) => `${startingCoordinate.split("")[0]}${x}`);
+        }
       } else if (directionOfPlacement == "vertical") {
         /**
          * if vertical then when traversing
          * the board to place the ship
+         *
          * the number stays the same
          * like d2 as a starting point would
          * still have (someletter)2 as an end point
+         *
          * I could assume that all ships being placed
          * vertically are having the bottom of the ship
          * placed on the startingCoordiante such that
          * the rest of the ship is being placed above that
          * cooridante so the letter just have to be incremented
+         *
          * if length of ship is 3 and starting point is at letter
-         * g then it will go up to letter I g(x) to i(x)
+         * g then it will go up to letter I, g(x) to i(x)
+         *
          * use ['A','B','C','D','E','F','G','H','I','J']
          * this data structure to traverse x steps up
-         * from the start to find the end point and each
-         * inbetween along the way to the end point
-         * add those coords to a secondary array
-         * then loopthrough them to update the
-         * coordiantes key in the game board
-         * will need to have error handleing for
-         * when the ship can't be place
-         * due to position and length of ship
-         * it will happen when looping through the
-         * letters array above
-         * also need to check the second array of
-         * filled with coordiantes to see if
-         * those cooridnates are already occupied
-         *
-         * what should the update value be?
-         * -a reference to the ship that lives there?
+         * from the start to find the end point
          *
          */
+
+        let startingCoordianteLetter = startingCoordinate.split("")[0];
+
+        let startingCoordianteLetterPosition =
+          startingCoordianteLetter.charCodeAt(0) - 65;
+
+        calculatedEndCoordinate =
+          startingCoordianteLetterPosition + lengthOfShip;
+
+        /**
+         * check for out of bounds:
+         * due to position and length of ship
+         */
+        if (calculatedEndCoordinate > 10) {
+          isOutOfBounds = true;
+        }
+
+        if (isOutOfBounds) {
+          //return error or throw error
+          /**do error handling (need to learn how.) */
+          throw "out of bounds ship can't be placed there";
+        } else {
+          calculatedCoordinateRange = Array.from(Array(10))
+            .map((e, i) => i + 65)
+            .map((x) => String.fromCharCode(x))
+            .slice(startingCoordianteLetterPosition, calculatedEndCoordinate)
+            .map((x) => `${x}${startingCoordinate.split("")[1]}`);
+        }
       }
+
       /**
-       * user can only click in a single
-       * sqaure on the board to place the
-       * ship the only other option
-       * is to rotate to place horizontally
-       * or vertically.
-       * from a starting locatin like
-       * a5 or g2 and the orientation
-       * horizational or vertical
-       * plus the number of coordinates
-       * the ship will occupie based on
-       * it's length. I need to figure
-       * out a general soultion to find
-       * those cooridnates and then up
-       * date them in a way that reflects
-       * that a ship is there
-       * and if that spot is already occupied
-       * it should'nt let the ship be placed
-       * also if the ship being turned cause
-       * it to not be placed on the grid
-       * due to postion and it's length conflicting
-       * it shouldn't be placed
+       * check if there is already a ship
+       * on any of the coords found
+       * if each location is unoccupied
+       * then place ship replace unoccupied
+       * with using  ship(lengthOfShip)
        */
+      let shipToPlace = ship(lengthOfShip);
+      let isUnocupied = calculatedCoordinateRange.every(
+        (x) => this.coordinatesOfMyShips[x] == "unoccupied"
+      );
+      if (isUnocupied) {
+        /**placing ship in coordinatesOfMyShips
+         * a reference to the shipToPlace will be
+         * available at each coordiante it's located
+         */
+        calculatedCoordinateRange.forEach(
+          (x) => (this.coordinatesOfMyShips[x] = shipToPlace)
+        );
+      } else {
+        throw "placement is ocupied ship can't be place there";
+      }
     },
 
     /**
@@ -171,37 +196,36 @@ function gameBoard() {
      * keep track of missed attacks and display them
      * maybe replace missedAttacks key with the
      * a coordiantes object
-    */
-    missedAttack:[],
+     */
+    missedAttack: [],
 
     //
-    displayMissedAttacks(){
-        /**
-         * need to make a second second grid object
-         * similar to coordiantes for recording
-         * hit's or misses on the Enemy
-         */
+    displayMissedAttacks() {
+      /**
+       * need to make a second second grid object
+       * similar to coordiantes for recording
+       * hit's or misses on the Enemy
+       */
     },
-    
 
     /**
-    * report if all ships have been sunk
-    */
-    reportIfAllShipsSunk(){
-        /**
-         * could look through the coordiantes that
-         * contain the players ships and find all ships
-         * on that grid and check if each ship found
-         * is sunk or not by checking it's prop hasBeenSunk
-         * or create a key on the gameBoard that contains
-         * all ships placed and just call a . every or some
-         * other array method to see if all element in it
-         * pass the condtion of hasBeenSunk = false
-         * if(this.shipsArray.every((x)=>{x.hasBeenSunk == false})){
-         *  all ships have been sunk game over...
-         * report...?
-         * }
-         */
+     * report if all ships have been sunk
+     */
+    reportIfAllShipsSunk() {
+      /**
+       * could look through the coordiantes that
+       * contain the players ships and find all ships
+       * on that grid and check if each ship found
+       * is sunk or not by checking it's prop hasBeenSunk
+       * or create a key on the gameBoard that contains
+       * all ships placed and just call a . every or some
+       * other array method to see if all element in it
+       * pass the condtion of hasBeenSunk = false
+       * if(this.shipsArray.every((x)=>{x.hasBeenSunk == false})){
+       *  all ships have been sunk game over...
+       * report...?
+       * }
+       */
     },
   };
 }
