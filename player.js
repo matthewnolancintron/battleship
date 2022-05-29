391b17a11dad7208ee38a7b559b5bcf459b2bec7
Matthew Shane Nolan-Cintron <matthewnolancintron@gmai.com>
2022-05-29 13:29:32 -0400

    working on the player and it's tests

--- player.js
+++ player.js
@@ -1,21 +1,67 @@
-function player(){
+function player(type) {
+  switch (type) {
+    case "human":
+      //human player object
+      return {
+        /**
+         * what properties and methods
+         * should the human player have
+         *
+         * Properties:
+         * create an association between the player
+         * and the gameBoard
+         * Each player gets one gameBoardObject
+         * store gameBoardObject as a key on the player object
+         *
+         * should I create a key for keeping track of players turn?
+         *
+         * methods:
+         * //ability to attack the Enemy gameBoard
+         * attackEnemyGameBoard();
+         * //maybe a method to set turn to finished?
+         * //maybe add more method if needed later.
+         *
+         */
+      };
+      break;
+    case "AI":
+      //AI player Object
+      return {
+        /**
+         * what properties and methods
+         * should the AI player have
+         *
+         * about the same as the human player expect
+         * the attack enemy ship method is different
+         * in that it will hit random legal moves
+         * and human will play what is selected
+         * or passed in to the function from the person
+         * playing.
+         */
+        /**
+         * The game is played against the computer,
+         *  so make ‘computer’ players capable of
+         *  making random plays.
+         *  The AI does not have to be smart,
+         * but it should know whether or
+         *  not a given move is legal.
+         *  (i.e. it shouldn’t shoot
+         *  the same coordinate twice).
+         */
+        //ability to attack the Enemy gameBoard
+      };
+      break;
 
-    //no idea how to make this function yet.
+    default:
+      break;
+  }
 
-    /**Players can take turns
+  /**
+   * Players can take turns
      *playing the game by attacking
-    the enemy Gameboard. */
+    the enemy Gameboard.
 
-    /**
-     * The game is played against the computer,
-     *  so make ‘computer’ players capable of
-     *  making random plays.
-     *  The AI does not have to be smart, 
-     * but it should know whether or
-     *  not a given move is legal.
-     *  (i.e. it shouldn’t shoot
-     *  the same coordinate twice).
+    how to implement turn taking?
      */
-
 }
 module.exports = player;
--- player.test.js
+++ player.test.js
@@ -1,5 +1,13 @@
 const player = require('./player');
 
-test('not sure what to test yet', ()=>{
+test('players can take turns attacking each other', ()=>{
     expect(player()).toBe()
-})
+    const spy = jest.spyOn(/**some player Object */,/**a method from the player object being spyed on*/);
+
+    //all ship should not be sunk
+    const /**a return value from a method call*/ = playersGameBoard./*a method being called*/();
+
+    expect(spy).toHaveBeenCalled();
+      
+    expect(/**the variable holding the return value from the method call*/).toStrictEqual(/**some expectation*/);
+});
