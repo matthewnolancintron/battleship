8fd3c95440305f1cc77879debd8564419da7ec5e
Matthew Shane Nolan-Cintron <matthewnolancintron@gmai.com>
2022-06-09 14:01:55 -0400

    working on the interface for the ship placement options

--- domInteraction.js
+++ domInteraction.js
@@ -1,18 +1,13 @@
 function domInteractions(human, AI) {
-  /**
-          display both the player’s 
-          boards and render them using
-          information from the Gameboard class. 
-    */
   return {
+    /**
+            display both the player’s 
+            boards and render them using
+            information from the Gameboard class. 
+    */
+
     //playerType for development only
     displayGameBoardForPlayer(player, playerType) {
-      /*
-      make 2 grids:
-      one for player ships
-      other for attacking enemy ships
-      */
-
       /**
       opponets grid that doesn't show any ships
       until the player hits the ship or will display 
@@ -45,61 +40,23 @@ function domInteractions(human, AI) {
       //conditionals for playerType is for development only
       if (playerType == "human") {
         /**
-         * add ship placement option for player
-         * manual drag and drop or random position button
-         * drag in drop is in development so just use random button
-         * for now
-         * once game starts ship placement buttons are removed
-         * from dom until next game/match
-         */
-        /**
-         * create button called randomize placement
-         * and another called manual placement
-         * place buttons under your grid
-         */
-        /**
-         * randomize placement button:
-         * when pressed will generate armada of ship elements
-         * from the player object data and then place them onto
-         * the correct locaction on the grid element using the
-         * player.playersBoard.coordinateOfMyShips data.
-         */
-
-        /**
-         * manaul placement:
-         * when pressed will genereate armada of ship elements
-         * from the player object data
-         * the ship elements will be placed to the left of the grid
-         * for the user to then drag and drop on to the grid located
-         * on the right.
-         *
-         * ship element can only be placed on grid element
-         * if it can fit and doesn't intersect other element
-         * and must have at least one free space bewtween it
-         * and another ship so ships can be touching other ships
-         * or bordering each other need at least a gap of one square
-         * grid unit between ships before droping onto the grid
-         *
-         * during the placement of ships the gameboard will only display
-         * yourShips grids and the ship elements to be placed
-         * opponets grid will not be shown
-         *
-         * once all ships are placed added to the grid there will be
-         * a button under the grid for confirming the placement
-         *
-         * then the grid with the ships placed as selected by the user
-         * will display to the left and then the empty grid for attacks
-         * on the opponent wil display to the right and the game
-         * will be ready to start.
-         */
-
-        /**
          * add grids to correct location in the DOM
          */
         document.getElementById("gridOfHumansShips").append(gridForPlayerShips);
         document
           .getElementById("gridOfHumansEnemysShips")
           .append(gridForAttacksOnEnemyShips);
+
+        //add ships to gridForPlayerShips
+        /**
+         * use generateShipPlacementOptions function
+         */
+        let shipPlaceMentOptions = generateShipPlacementOptions(player);
+        //add shipPlacementOption
+        // to gridOfHumansShips container element
+        document
+          .getElementById("gridOfHumansShips")
+          .append(shipPlaceMentOptions);
       }
 
       if (playerType == "A.I") {
@@ -127,65 +84,58 @@ function domInteractions(human, AI) {
 }
 
 function generateGridElement() {
-  console.log("generate grid?");
-  let gridContainer = document.createElement('div');
-  gridContainer.classList.add('gridContainer')
+  let gridContainer = document.createElement("div");
+  gridContainer.classList.add("gridContainer");
   /**
    * top row horizonatal is labeled a-j
    * vertical column off to the left is labeled 1-10
    */
   let gridColumnLabels = [];
   let gridRowLabels = [];
+
+  //attach method or event to each coordinate?
+  //or just add method to dom object and have
+  //dom object get the coordiante?
   let gridCoordiantes = [];
+
   //letters are the columns
-  [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-  ].forEach(c=>{
-    let gridColumnLabel = document.createElement('p');
+  ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"].forEach((c) => {
+    let gridColumnLabel = document.createElement("p");
     gridColumnLabel.textContent = c;
-    gridColumnLabel.setAttribute('data-column', c);
-    
+    gridColumnLabel.setAttribute("data-column", c);
+
     //todo add class
-    gridColumnLabel.classList.add('gridColumnLabel');
+    gridColumnLabel.classList.add("gridColumnLabel");
 
     gridColumnLabel.style.gridArea = `${c}`;
 
     gridColumnLabels.push(gridColumnLabel);
 
     //generate cells
-    Array.from(Array(10).keys(),x=>x+1).forEach(r=>{
-      let gridCoordiante = document.createElement('div');
-      
-      //todo add class 
-      gridCoordiante.classList.add('gridCoordiante');
-      
+    Array.from(Array(10).keys(), (x) => x + 1).forEach((r) => {
+      let gridCoordiante = document.createElement("div");
+
+      //todo add class
+      gridCoordiante.classList.add("gridCoordiante");
+
       //
-      gridCoordiante.setAttribute('data-coordiante', `${c}${r}`);
+      gridCoordiante.setAttribute("data-coordiante", `${c}${r}`);
 
       gridCoordiante.style.gridArea = `${c}${r}`;
 
       gridCoordiantes.push(gridCoordiante);
-    });  
+    });
   });
-  
-  //numbers are the rows
-  Array.from(Array(10).keys(),x=>x+1).forEach((x,i)=>{
-    let gridRowLabel = document.createElement('p');
-    gridRowLabel.textContent = x;
-    
-    //todo add class
-    gridRowLabel.classList.add('gridRowLabel');
 
-    gridRowLabel.setAttribute('data-row',x);
+  //numbers are the rows
+  Array.from(Array(10).keys(), (x) => x + 1).forEach((x, i) => {
+    let gridRowLabel = document.createElement("p");
+    gridRowLabel.textContent = x;
+
+    //todo add class
+    gridRowLabel.classList.add("gridRowLabel");
+
+    gridRowLabel.setAttribute("data-row", x);
 
     gridRowLabel.style.gridArea = `X${x}`;
 
@@ -196,23 +146,118 @@ function generateGridElement() {
   // console.log(gridRowLabels);
   // console.log(gridCoordiantes);
 
-  gridColumnLabels.forEach(c=>{
+  gridColumnLabels.forEach((c) => {
     gridContainer.append(c);
   });
 
-  gridRowLabels.forEach(r=>{
+  gridRowLabels.forEach((r) => {
     gridContainer.append(r);
   });
 
-  gridCoordiantes.forEach(x => {
+  gridCoordiantes.forEach((x) => {
     gridContainer.append(x);
   });
 
   return gridContainer;
-};
+}
 
-function addShipPlacementOption(){
-  
+function generateShipPlacementOptions(player) {
+  /**
+   * add ship placement option for player
+   * manual drag and drop or random position button
+   * drag in drop is in development so just use random button
+   * for now
+   * once game starts ship placement buttons are removed
+   * from dom until next game/match
+   */
+  /**
+   * create button called randomize placement
+   * and another called manual placement
+   * place buttons under your grid
+   */
+  /**
+   * randomize placement button:
+   * when pressed will generate armada of ship elements
+   * from the player object data and then place them onto
+   * the correct locaction on the grid element using the
+   * player.playersBoard.coordinateOfMyShips data.
+   */
+
+  /**
+   * manaul placement:
+   * when pressed will genereate armada of ship elements
+   * from the player object data
+   * the ship elements will be placed to the left of the grid
+   * for the user to then drag and drop on to the grid located
+   * on the right.
+   *
+   * ship element can only be placed on grid element
+   * if it can fit and doesn't intersect other element
+   * and must have at least one free space bewtween it
+   * and another ship so ships can be touching other ships
+   * or bordering each other need at least a gap of one square
+   * grid unit between ships before droping onto the grid
+   *
+   * during the placement of ships the gameboard will only display
+   * yourShips grids and the ship elements to be placed
+   * opponets grid will not be shown
+   *
+   * once all ships are placed added to the grid there will be
+   * a button under the grid for confirming the placement
+   *
+   * then the grid with the ships placed as selected by the user
+   * will display to the left and then the empty grid for attacks
+   * on the opponent wil display to the right and the game
+   * will be ready to start.
+   */
+
+  let shipPlacementOptionsContainer = document.createElement("div");
+  shipPlacementOptionsContainer.classList.add("shipPlacementOptionsContainer");
+
+  let shipPlacementMessage = document.createElement("p");
+  shipPlacementMessage.textContent = "How do you want to place your ships?";
+  shipPlacementMessage.classList.add("shipPlacementMessage");
+
+  let placeShipsRandomlyOptionButton = document.createElement("button");
+  placeShipsRandomlyOptionButton.classList.add("randomPlacementOption");
+  placeShipsRandomlyOptionButton.textContent = "randomise";
+
+  let placeShipsManualOptionButton = document.createElement("button");
+  placeShipsManualOptionButton.classList.add("manualPlacementOption");
+  placeShipsManualOptionButton.textContent = "manual";
+
+  placeShipsRandomlyOptionButton.addEventListener("click", () => {
+    //need to clear placement before randomizing it again.
+    player.playersBoard.clearShipPlacements();
+
+    //
+    player.playersBoard.randomiseShipPlacements();
+    console.log(player.playersBoard.armada, "humans ships");
+    console.log(
+      player.playersBoard.coordinatesOfMyShips,
+      "placement of ships for human"
+    );
+
+
+    // will need to generate ship element too...
+    /**
+     * todo: create function that generates
+     * ship elements each call will generate
+     * 1 set of ships a whole ship armada.
+     * and return and array of those ships elements.
+     */
+  });
+
+  // todo create manual ship placementEvent.
+  placeShipsManualOptionButton.addEventListener("click", () => {});
+
+  // add buttons to container
+  shipPlacementOptionsContainer.append(shipPlacementMessage);
+  shipPlacementOptionsContainer.append(placeShipsRandomlyOptionButton);
+  shipPlacementOptionsContainer.append(placeShipsManualOptionButton);
+
+  //return the container element
+  return shipPlacementOptionsContainer;
 }
 
 export default domInteractions;
